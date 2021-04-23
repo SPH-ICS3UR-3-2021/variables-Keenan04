e22420978c78535116033c47438d7bde408ed106
Keenan04 <keenan.nasrallah@ocsbstudent.ca>
2021-04-23 13:07:33 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,18 @@
  */
 public class Main {
     public static void main(String[] args){
-        
+    String x = "Hi my name is keenan! what is your name?";
+    System.out.println(x);
+    int z = 2004;
+    System.out.println (z);
+    boolean k = true;
+    System.out.println (k);
+    double J = 1.14159265 ; 
+    System.out.println (J) ;
+    float Myfloat = 1.12f ; 
+    System.out.println (Myfloat) ; 
+    char Mychar = '8' ; 
+    System.out.println(Mychar);
     }
     
 }
